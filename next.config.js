dbec1fa2a57ba5577b5cc7b5b92756fef72b92f6
AbdelRhman Samy <abdojoey17@gmail.com>
2023-12-05 01:56:18 +0200

    final commit

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { hostname: "localhost", pathname: "*", port: "3000", protocol: "http" },
+    domains:[
+      "localhost",
+      "digitalhippo-production-a35e.up.railway.app"
     ],
+    // remotePatterns: [
+    //   { hostname: "localhost", pathname: "*", port: "3000", protocol: "http" },
+    // ],
   },
 };
 
